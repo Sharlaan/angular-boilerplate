72b1c36041c63c266f74f7aeaa24dad2be100f03
Raphaël Morineau <raphael.morineau@gmail.com>
2022-07-02 13:24:12 +0200

    Set public.private routes pattern

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Public routes
+  // { path: 'login', component: LoginComponent },
+
+  // Private routes
+  { path: 'app', component: AppComponent /*, canActivate: [AuthGuard]*/ },
+
+  { path: '**', redirectTo: 'app' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
